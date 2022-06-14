921f2a652e5c026facb2ead5bbf0c4f1431e56e0
AllanKaio21 <allankaio276@gmail.com>
2022-06-13 21:00:36 -0300

    update

--- genGrade.py
+++ genGrade.py
@@ -6,43 +6,42 @@ from random import randint
 def Grid(shape):
     n = shape[0] * shape[1]
     nodes = []
-    # iterate through all combinations of coordinates
     for x in range(shape[0]):
         for y in range(shape[1]):
-            # make and all possible neighbors
             if y == 0:
-                print("1")
                 if x != shape[0]-1:
                     neighbors = [(x + 1, y), (x, y + 1), (x + 1, y + 1)]
                 else:
                     neighbors = [(x, y + 1)]
             elif y == shape[1]-1 and x != shape[0]-1:
-                print("2")
                 neighbors = [(x + 1, y), (x + 1, y - 1)]
             elif x == shape[0]-1:
-                print("3")
                 if y != shape[1]-1:
                     neighbors = [(x, y + 1)]
             else:
-                print("4")
                 neighbors = [(x + 1, y - 1), (x + 1, y), (x, y + 1), (x + 1, y + 1)]
-            print(x, ",", y, " ", neighbors)
+            # print(x, ",", y, " ", neighbors)
             nodes.append(neighbors)
 
     tr = {}  # type: typing.Dict[tuple, list]
     cont = 0
-
+    # Conta vertices
     for x in range(shape[0]):
         for y in range(shape[1]):
+            # print((x, y), cont)
             tr[(x, y)] = cont
             cont += 1
-    E = set()
+
+    E = []
     c = 0
     for v in nodes:
         for u in v:
-            E.add((c, tr[u]))
+            # print(c, tr[u])
+            if c != tr[u]:
+                E.append((c, tr[u]))
         c += 1
-
+    # print(E)
+    # Gera o grafo
     g = grafo()
     V = [i for i in range(n)]
     for i in V:
--- main.py
+++ main.py
@@ -6,53 +6,52 @@ from grafo import grafo
 
 
 def main():
+    g = None
+    print("Selecione uma Opção: ")
+    print("1 - Inserir grafo manualmente")
+    print("2 - Gerar grafo aleatorio")
+    print("3 - Gerar grafo de grade")
+    print("3 - Sair")
+    a = input("=> ")
 
-    g = Grid((6, 6))
+    if a == '1':
+        g = manual()
+    elif a == '2':
+        v = input("Digite quantidade de vertices: ")
+        p = input("Digite probabilidade: ")
+        d = input("Digite se é dirigidor True / False: ")
+        g = ER(int(v), float(p), bool(d))
+    elif a == '3':
+        p1 = input("Digite proporção: ")
+        p2 = input("Digite proporção: ")
+        g = Grid((int(p1), int(p2)))
+    else:
+        print("Bye!")
+        return 1
 
+    o = input("Escolha vertice de origem: ")
+    d = input("Escolha vertice de destino: ")
+    res = bellman_ford(g[0], int(o), int(d))
+    if res:
+        for i in res:
+            print("Caminho: ", i[0], " Custo: ", i[1])
+            plotCaminho(g[1], g[2], i[0])
+    else:
+        print("Não existe caminho minimo!")
+
+    # Plota o grafo
     plotGrafo(g[1], g[2])
 
-    # g = None
-    # print("Selecione uma Opção: ")
-    # print("1 - Inserir grafo manualmente ")
-    # print("2 - Gerar grafo aleatorio")
-    # print("3 - Sair")
-    # a = input("=> ")
-    #
-    # if a == '1':
-    #     g = manual()
-    # elif a == '2':
-    #     v = input("Digite quantidade de vertices: ")
-    #     p = input("Digite probabilidade: ")
-    #     d = input("Digite se é dirigidor True / False: ")
-    #     g = ER(int(v), float(p), bool(d))
-    # else:
-    #     print("Bye!")
-    #     return 1
-    #
-    # o = input("Escolha vertice de origem: ")
-    # d = input("Escolha vertice de destino: ")
-    # res = bellman_ford(g[0], int(o), int(d))
-    # if res:
-    #     for i in res:
-    #         print("Caminho: ", i[0], " Custo: ", i[1])
-    #         plotCaminho(g[1], g[2], i[0])
-    # else:
-    #     print("Não existe caminho minimo!")
-    #
-    # # Plota o grafo
-    # plotGrafo(g[1], g[2])
-    #
-    # # Matriz Adjacencia
-    # Madj = matrizAdjacencia(g[0])
-    #
-    # # Matriz de Custo2
-    #
-    # Mcusto = matrizDeCusto(g[0])
-    #
-    # # Plota as matrizes
-    # plotMatriz(Madj, "Matriz de Adjacencia")
-    # plotMatriz(Mcusto, "Matriz de Custo")
+    # Matriz Adjacencia
+    Madj = matrizAdjacencia(g[0])
 
+    # Matriz de Custo2
+
+    Mcusto = matrizDeCusto(g[0])
+
+    # Plota as matrizes
+    plotMatriz(Madj, "Matriz de Adjacencia")
+    plotMatriz(Mcusto, "Matriz de Custo")
 
 if __name__ == '__main__':
     main()
